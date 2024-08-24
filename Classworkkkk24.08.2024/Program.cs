8145c34e032794735f440f6bdf68306a80ef996a
ILLIA <ILLIA@DESKTOP-0AR0TKH>
2024-08-24 11:48:09 +0300

    Add project files.

---
+++ Classworkkkk24.08.2024/Program.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace ShopEntityFramework
+{
+    class Position
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    class Worker
+    {
+
+    }
+    class Shop
+    {
+
+    }
+    class City
+    {
+
+    }
+    class Country
+    {
+
+    }
+    class Product
+    {
+
+    }
+    class Category
+    {
+
+    }
+    class ShopDbContext : DbContext
+    {
+        public DbSet<Position> Positions { get; set; }
+        public DbSet<Worker> Workers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Shop> Shops { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
